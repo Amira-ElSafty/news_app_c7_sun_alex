31544c86c9e81e951ba7ac7b9d900f9e368eec64
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2022-12-10 20:15:18 +0200

    initial project. empty home screen.

---
+++ android/app/src/main/kotlin/com/example/flutter_news_app_sun_c7_alex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_news_app_sun_c7_alex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app_sun_c7_alex/home/home_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName : (context) => HomeScreen(),
+      },
+    );
+  }
+}
